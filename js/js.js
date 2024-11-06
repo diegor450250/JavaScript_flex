1aa42c27dd79742c6f186553c2df3690926d6681
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2024-11-06 13:23:56 -0600

    Cambio en la funcion historial y borrar historial

--- js/js.js
+++ js/js.js
@@ -1,73 +1,72 @@
-        const historial = [];
+document.addEventListener('DOMContentLoaded', saludar() );
 
-        document.addEventListener('DOMContentLoaded', saludar() );
-
-        function saludar (){
-            const nombre = prompt("Hola cual es tu nombre");
-            if (nombre == '' || nombre == null) {
+function saludar (){
+    const nombre = prompt("Hola cual es tu nombre");
+    if (nombre == '' || nombre == null) {
                 alert("Es un gusto tenerte por aqui desconocido");
                 document.getElementById('saludo').innerText = 'Bienvenido desconocido!';
-            } else {
+    } else {
                 alert("Es un gusto tenerte por aqui " + nombre);
                 document.getElementById('saludo').innerText = 'Bienvenido ' + nombre + '!';
-            }
-        }
-        
+    }
+}        
 
-        function AddHistorial(num1, num2, result, operacion) {
-           let operador = " " + operacion + " ";
-            historial.push(num1 + operador + num2 + " = " + result);
-            document.getElementById('historial').innerText = historial;
-        }
+function AddHistorial(num1, num2, result, operacion) {
+    const nuevoP = document.createElement("p");
+    nuevoP.innerText = num1 + " " + operacion + " " + num2 + " = " + result;
+    nuevoP.classList.add('historial')
+    const contenedor = document.getElementById("contenedor-historial");
+    contenedor.appendChild(nuevoP);
+}
 
-        function sumar() { 
-            let num1 = parseInt(document.getElementById('num1').value);
-            let num2 = parseInt(document.getElementById('num2').value);
-            let result = num1 + num2;
-            document.getElementById('resultado').innerText = "El resultado es " + result;
-            AddHistorial(num1, num2, result, "+")
+function sumar() { 
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let result = num1 + num2;
+    document.getElementById('resultado').innerText = "El resultado es " + result;
+    AddHistorial(num1, num2, result, "+")
 
-        }
+}
 
-        function resta() {
-            let num1 = parseInt(document.getElementById('num1').value);
-            let num2 = parseInt(document.getElementById('num2').value);
-            let result = num1 - num2;
-            document.getElementById('resultado').innerText = "El resultado es " + result;
-            AddHistorial(num1, num2, result, "-")
-        }
+function resta() {
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let result = num1 - num2;
+    document.getElementById('resultado').innerText = "El resultado es " + result;
+    AddHistorial(num1, num2, result, "-")
+}
 
-        function multiplicar() {
-            let num1 = parseInt(document.getElementById('num1').value);
-            let num2 = parseInt(document.getElementById('num2').value);
-            let result = num1 * num2;
-            document.getElementById('resultado').innerText = "El resultado es " + result;
-            AddHistorial(num1, num2, result, "*")
-        }
+function multiplicar() {
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let result = num1 * num2;
+    document.getElementById('resultado').innerText = "El resultado es " + result;
+    AddHistorial(num1, num2, result, "*")
+}
 
-        function dividir() {
-            let num1 = parseInt(document.getElementById('num1').value);
-            let num2 = parseInt(document.getElementById('num2').value);
-            let result = num1 / num2;
-            document.getElementById('resultado').innerText = "El resultado es " + result;
-            AddHistorial(num1, num2, result, "/")
-        }
+function dividir() {
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let result = num1 / num2;
+    document.getElementById('resultado').innerText = "El resultado es " + result;
+    AddHistorial(num1, num2, result, "/")
+}
 
-        function borrarHistorial() {
-            historial.length = 0;
-            document.getElementById('historial').innerText = historial;
-        }
+function borrarHistorial() {
+    const pHitsorial = document.querySelectorAll('.historial');
+    pHitsorial.forEach(parrafo => parrafo.remove());
+}
 
-        function validarInput(callback) {
-            let num1 = document.getElementById('num1').value;
-            let num2 = document.getElementById('num2').value;
-            if (num1.length == ''|| num2.length == '') {
-                alert("Llena todos los campos");
-            } else {
-                callback();
-            }
-        }
+function validarInput(callback) {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    if (num1.length == ''|| num2.length == '') {
+        alert("Llena todos los campos");
+    } else {
+        callback();
+    }
+}
 
-        function modoNoche() {
-            document.body.classList.toggle('night-mode');
-        }
+function modoNoche() {
+    document.body.classList.toggle('night-mode');
+}
